94a47d5970e91f26e8680886429af6318bf4c505
valemosca00 <vmosca22@fed.idserve.net>
2022-10-20 09:18:40 +0200

    Initial Commit

---
+++ Magn/MagnApp.swift
@@ -0,0 +1,17 @@
+//
+//  MagnApp.swift
+//  Magn
+//
+//  Created by Valerio Mosca on 20/10/22.
+//
+
+import SwiftUI
+
+@main
+struct MagnApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
