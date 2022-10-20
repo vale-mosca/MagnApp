d0164d93e0156b52b852f01579b589bbe5484626
valemosca00 <vmosca22@fed.idserve.net>
2022-10-20 11:35:50 +0200

    starting

--- Magn/ContentView.swift
+++ Magn/ContentView.swift
@@ -9,15 +9,32 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-            Text("pat't")
+        VStack(spacing: 20.0) {
+            Image(systemName:"profile.fill")
+            
+            Text("Hello Chef!")
+                .font(.largeTitle)
+                .fontWeight(.bold)
+                .foregroundColor(Color.black)
+            
+            Text("Let's create something tasty.")
+                .font(.subheadline)
+                .foregroundColor(Color.black)
+            
+            Text("Suggested")
+                .font(.title)
+                .fontWeight(.bold)
+                .foregroundColor(Color.black)
+            
+            List {
+                /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Content@*/Text("Content")/*@END_MENU_TOKEN@*/
+            }
+            
+            Spacer()
             
         }
-        .padding()
+        .frame(maxWidth: .infinity)
+            .background(Color(red: 242/255, green: 241/255, blue: 246/255))
     }
 }
 
