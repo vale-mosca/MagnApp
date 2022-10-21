b6d4c19f1bdaf648ab7d301a9beb8599dc3643bc
valemosca00 <vmosca22@fed.idserve.net>
2022-10-21 13:06:16 +0200

    add list
    added list of ingridients

---
+++ Magn/RelatedRecipies.swift
@@ -0,0 +1,20 @@
+//
+//  RelatedRecipies.swift
+//  Magn
+//
+//  Created by Valerio Mosca on 21/10/22.
+//
+
+import SwiftUI
+
+struct RelatedRecipies: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct RelatedRecipies_Previews: PreviewProvider {
+    static var previews: some View {
+        RelatedRecipies()
+    }
+}
