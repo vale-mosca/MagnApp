46b525e74efe8f0087c906fe9987134e8b2bf2ed
valemosca00 <vmosca22@fed.idserve.net>
2022-10-24 12:07:13 +0200

    Adding models

---
+++ Magn/Models/Model.swift
@@ -0,0 +1,34 @@
+//
+//  Model.swift
+//  Magn
+//
+//  Created by Sao Paulo on 20/10/22.
+//
+
+import Foundation
+
+
+struct Step{
+    var Image: String //prendiamo dall'assets
+    var Azione: String
+    var Descrizione: String
+}
+
+struct Allergen{
+    var Name: String
+    var Image: String
+}
+
+struct Recipe {
+    var Name: String
+    var Description: String
+    var Image: String
+    var IngredientList: Array<String>
+    var CookTime: Int
+    var PrepTime: Int
+    var Steps: Array<Step>
+    var Allergens: Array<Allergen>
+    
+}
+
+var caprese = Recipe(Name: <#T##String#>, Description: <#T##String#>, Image: <#T##String#>, IngredientList: <#T##Array<String>#>, CookTime: <#T##Int#>, PrepTime: <#T##Int#>, Steps: <#T##Array<Step>#>, Allergens: <#T##Array<Allergen>#>)
